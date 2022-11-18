fd1c272085bc3644029333ca6b15b44a876cc1e3
Ekaterina Ivanova <katenika158@gmail.com>
2022-11-18 22:35:16 +0000

    chore: Add Item db model

--- stripeApp/models.py
+++ stripeApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=250)
+    description = models.TextField(blank=True, null=True, max_length=500)
+    price = models.IntegerField()
+
+    class Meta:
+        managed = True
+        db_table = 'items'
