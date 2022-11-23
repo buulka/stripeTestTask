0c88c08b5c767b282a65e437fef75543787bef9d
Ekaterina Ivanova <katenika158@gmail.com>
2022-11-23 00:53:03 +0000

    chore: Add Discount and Tax models to admin page

--- stripeApp/admin.py
+++ stripeApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Item, Order
+from .models import Item, Order, Discount, Tax
 
 
-admin.site.register([Item, Order])
+admin.site.register([Item, Order, Discount, Tax])
