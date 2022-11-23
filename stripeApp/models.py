4c91de8145914af6c8fd9f16f0fa986e2244dc41
Ekaterina Ivanova <katenika158@gmail.com>
2022-11-23 00:38:49 +0000

    chore: Add Discount and Tax models

--- stripeApp/models.py
+++ stripeApp/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 
 
+class Discount(models.Model):
+    id = models.IntegerField(primary_key=True)
+    amount = models.IntegerField(blank=False, null=False)
+
+    class Meta:
+        managed = True
+        db_table = 'discounts'
+        verbose_name_plural = 'Скидки'
+
+
+class Tax(models.Model):
+    id = models.IntegerField(primary_key=True)
+    amount = models.IntegerField(blank=False, null=False)
+
+    class Meta:
+        managed = True
+        db_table = 'taxes'
+        verbose_name_plural = 'Налоги'
+
+
 class Item(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=250)
@@ -16,8 +36,11 @@ class Item(models.Model):
 class Order(models.Model):
     id = models.IntegerField(primary_key=True)
     items = models.ManyToManyField(Item)
+    discount = models.ForeignKey(Discount, on_delete=models.SET_NULL, null=True)
+    tax = models.ForeignKey(Tax, on_delete=models.SET_NULL, null=True)
 
     class Meta:
         managed = True
         db_table = 'orders'
         verbose_name_plural = 'Заказы'
+
